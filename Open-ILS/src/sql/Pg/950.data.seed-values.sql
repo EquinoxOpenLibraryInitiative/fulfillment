6d63c3f32c2dba0c90ac5b1d18144b9d5c91f532
Galen Charlton <gmc@equinoxOLI.org>
2022-12-15 01:24:05 +0000

    Koha connector: add three new settings
    The Koha connector needs to use both the legacy /svc API (to create
    temporary bibs and items) and its RESTful API. Since the RESTful
    API uses OAuth-derived tokens and is accessed from the OPAC host
    but the /svc API uses username/passwords and is accessed from the staff
    interface host, we need the extra settings.
    
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -17105,6 +17105,18 @@ INSERT INTO config.org_unit_setting_type ( name, label, description, datatype ) 
 (   'ff.remote.connector.passwd.item',
     oils_i18n_gettext( 'ff.remote.connector.passwd.item', 'LAI: Connector Item Data Password', 'coust', 'label'),
     oils_i18n_gettext( 'ff.remote.connector.passwd.item', 'Password to be used with the owing site''s Connector', 'coust', 'description'),
+    'string'),
+(   'ff.remote.connector.extra.svc.host',
+    oils_i18n_gettext( 'ff.remote.connector.extra.svc.host', 'LAI: Connector svc API host (for Koha)', 'coust', 'label'),
+    oils_i18n_gettext( 'ff.remote.connector.extra.svc.host', 'Hostname of the Koha /svc API (usually the staff interface)', 'coust', 'description'),
+    'string'),
+(   'ff.remote.connector.extra.svc.user',
+    oils_i18n_gettext( 'ff.remote.connector.extra.svc.user', 'LAI: Connector svc API user (for Koha)', 'coust', 'label'),
+    oils_i18n_gettext( 'ff.remote.connector.extra.svc.user', 'User to be used to log into the Koha /svc API', 'coust', 'description'),
+    'string'),
+(   'ff.remote.connector.extra.svc.password',
+    oils_i18n_gettext( 'ff.remote.connector.extra.svc.password', 'LAI: Connector svc API password (for Koha)', 'coust', 'label'),
+    oils_i18n_gettext( 'ff.remote.connector.extra.svc.password', 'Password to be used to log into the Koha /svc API', 'coust', 'description'),
     'string');
 
 -- item auto-ingest related settings
